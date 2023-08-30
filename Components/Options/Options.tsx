43eae0e858d20335f81eb925040b89f175b8c847
Sango <60422984+MatyilaSango@users.noreply.github.com>
2023-08-30 21:24:19 +0200

    chore: added daily option data

--- Components/Options/Options.tsx
+++ Components/Options/Options.tsx
@@ -1,25 +1,33 @@
-import React, { MouseEvent } from 'react'
-import styles from "./Options.module.css"
+import React, { MouseEvent } from "react";
+import styles from "./Options.module.css";
 
 interface IhandleSetDailyOption {
-    handleSetDailyOption: (parameter: String) => void
+    handleSetDailyOption: (parameter: string) => void;
+    offsetHours: number;
 }
 
-export default function Options({ handleSetDailyOption }:  IhandleSetDailyOption){
+export default function Options({
+    handleSetDailyOption,
+    offsetHours,
+}: IhandleSetDailyOption) {
+    let arrayKeys: number[] = Array.from({ length: 12 }, (_, i) => {
+        let date = new Date();
+        date.setHours(date.getUTCHours() + offsetHours);
+        date.setDate(date.getDate() + i);
+        return date.getDate();
+    });
 
-    let arrayKeys: number[] = Array.from({length: 12}, (_,i) => i+1)
-
-    const handleOptionChange = (e: MouseEvent<HTMLSpanElement>): void => {
-        handleSetDailyOption(String(e.currentTarget.lastChild?.nodeValue))
-    }
+    const handleOptionChange = (e: number): void => {
+        handleSetDailyOption(String(e));
+    };
 
     return (
-    <div className={styles['Options-wrapper']}>
-        {arrayKeys.map(key_ => (
-            <span key={key_} onClick={handleOptionChange}>
-                {key_}
-            </span>
-        ))}
-    </div>
-    )
+        <div className={styles["Options-wrapper"]}>
+            {arrayKeys.map((key_, i) => (
+                <span key={key_} onClick={() => handleOptionChange(i + 1)}>
+                    {key_}
+                </span>
+            ))}
+        </div>
+    );
 }
