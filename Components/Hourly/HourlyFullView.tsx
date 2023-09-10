9b4a67ad835e815d375bfee3d49d4722188cb397
Sango <60422984+MatyilaSango@users.noreply.github.com>
2023-09-10 16:45:02 +0200

    chore: adde pic, time, and restructured

--- Components/Hourly/HourlyFullView.tsx
+++ Components/Hourly/HourlyFullView.tsx
@@ -13,9 +13,18 @@ export default function HourlyFullView({ setShowHourlyFullView, hourlyFullViewda
   return (
     <div className={styles["HourlyFullView-wrapper"]}>
       <div className={styles["HourlyFullView-wrapper__data-view-con"]}>
-        <div className={styles["HourlyFullView-wrapper__data-view-con__exit-btn"]} onClick={() => setShowHourlyFullView(false)}>
-          <Image src={clostImg} alt="close" width={30} height={30} />
+        <div className={styles["HourlyFullView-wrapper__data-view-con__pic-time-exit"]}>
+          <div className={styles["HourlyFullView-wrapper__data-view-con__pic-time-exit__pic"]}>
+            <Image src={hourlyFullViewdata.icon} alt="close" width={50} height={50} />
+          </div>
+          <div className={styles["HourlyFullView-wrapper__data-view-con__pic-time-exit__time"]}>
+            {hourlyFullViewdata.hour}
+          </div>
+          <div className={styles["HourlyFullView-wrapper__data-view-con__pic-time-exit__exit-btn"]} onClick={() => setShowHourlyFullView(false)}>
+            <Image src={clostImg} alt="close" width={30} height={30} />
+          </div>
         </div>
+
         <HourlyFullViewTable
           temp={hourlyFullViewdata.temp}
           real_feel={hourlyFullViewdata.real_feel}
@@ -33,7 +42,6 @@ export default function HourlyFullView({ setShowHourlyFullView, hourlyFullViewda
           cloudy_cover={hourlyFullViewdata.cloudy_cover}
           visibility={hourlyFullViewdata.visibility}
           cloud_ceiling={hourlyFullViewdata.cloudy_cover}
-          icon={hourlyFullViewdata.icon}
         />
       </div>
     </div>
