397caf74fb47d148ed4eff7fd7b5bc7f8e1b0d94
Sango <60422984+MatyilaSango@users.noreply.github.com>
2023-11-01 20:41:46 +0200

    chore: remove unncessary function call

--- Scapping/src/Controller/Location/Location.ts
+++ Scapping/src/Controller/Location/Location.ts
@@ -1,6 +1,6 @@
 import { locationObj } from "../../Addon/Objects/Objects";
 
-export default async function locationsHandler(query: string, getRootHTMLPage: (search: string) => Promise<any>) {
-  await locationObj.scrapLocations(query, getRootHTMLPage);
+export default async function locationsHandler(query: string) {
+  await locationObj.scrapLocations(query);
   return locationObj.getLocations();
 }
